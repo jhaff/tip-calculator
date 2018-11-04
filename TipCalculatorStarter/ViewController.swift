1597632709c1f0e0e25f1199752932e771817429
jhaff <jacobhaff2@gmail.com>
2018-11-04 12:45:43 -0800

    created setupViews() to style views adding rounded corners, borders, shadows.

--- TipCalculatorStarter/ViewController.swift
+++ TipCalculatorStarter/ViewController.swift
@@ -40,6 +40,26 @@ class ViewController: UIViewController {
         
     }
     
+    func setupViews() {
+        headerView.layer.shadowOffset = CGSize(width: 0, height: 1)
+        headerView.layer.shadowOpacity = 0.05
+        headerView.layer.shadowColor = UIColor.black.cgColor
+        headerView.layer.shadowRadius = 35
+        
+        outputCardView.layer.cornerRadius = 8
+        outputCardView.layer.masksToBounds = true
+        
+        inputCardView.layer.cornerRadius = 8
+        inputCardView.layer.masksToBounds = true
+        
+        // set output card border
+        outputCardView.layer.borderWidth = 1
+        outputCardView.layer.borderColor = UIColor.tcHotPink.cgColor
+        
+        resetButton.layer.cornerRadius = 8
+        resetButton.layer.masksToBounds = true
+    }
+    
     func clear() {
         billAmountTextField.text = nil
         tipPercentSegmentedControl.selectedSegmentIndex = 0
@@ -93,6 +113,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupViews()
         
         billAmountTextField.calculateButtonAction = {
             self.calculate()
